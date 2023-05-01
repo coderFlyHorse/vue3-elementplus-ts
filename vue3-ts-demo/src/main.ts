1d1a34ed91c979b70b3695d3b2b836f9f60c7bc6
bArter <1285434016@qq.com>
2023-05-01 18:17:38 +0800

    按需引入element plus

---
+++ vue3-ts-demo/src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+// 完整引入
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+createApp(App).use(router).mount('#app')
